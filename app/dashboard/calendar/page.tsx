6baabbad948f1618550ddd9708d61e31b079d3f9
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-06-09 17:15:09 -0300

    feat: aba de calendarios

---
+++ app/dashboard/calendar/page.tsx
@@ -0,0 +1,8 @@
+export default function CalendarPage(){
+    return(
+        <div>
+            <h1>Calendário</h1>
+            <p>Aqui você pode visualizar as atividades no calendário.</p>
+        </div>
+    )
+}
