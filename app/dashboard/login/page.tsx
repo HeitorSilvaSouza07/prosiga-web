66f2ed7b20a8ee9d939384194822891005bb3b1e
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-06-09 17:21:32 -0300

    feat: aba de login

---
+++ app/dashboard/login/page.tsx
@@ -0,0 +1,12 @@
+export default function LoginPage(){
+    return(
+        <div>
+            <div>
+                <h1>Login</h1>
+                <p>Aqui você pode fazer login para acessar o dashboard.</p>
+            </div>
+            <div>
+            </div>
+        </div>
+    )
+}
