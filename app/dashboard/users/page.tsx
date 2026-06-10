a6398841472529392936e15e8b5360ec617e4a08
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-06-10 19:30:14 -0300

    fix and feat

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,8 +1,39 @@
+"use client";
+import { useState, useEffect } from "react"
+
 export default function UsersPage(){
+
+    const [ users, setUsers ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    useEffect(() => {
+        async function buscaUsuarios(){
+            try{
+
+                const response = await fetch("http://localhost:3001/api/users");
+
+                const data = await response.json();
+
+                setUsers(data);
+
+            }catch(error){
+                console.error("Erro ao buscar usuários:", error);
+            }finally{
+                setLoading(false);
+            }
+        }
+        buscaUsuarios();
+    }, []);
+
     return(
         <div>
             <div>
-                                
+                {users.map((user) => (
+                    <div key={user.UserId}>
+                        <h3>{user.UserName}</h3>
+                        <p>{user.UserCpf}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
