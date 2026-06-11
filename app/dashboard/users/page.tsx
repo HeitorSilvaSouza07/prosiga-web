52931b90cb383bb6697a715aa2b7148598b8b76c
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-06-11 15:13:41 -0300

    feat: aba de usuarios

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,40 +1,64 @@
 "use client";
-import { useState, useEffect } from "react"
 
-export default function UsersPage(){
+import { useState, useEffect } from "react";
 
-    const [ users, setUsers ] = useState([]);
-    const [ loading, setLoading ] = useState(true);
+export default function UserPage() {
+
+    type User = {
+        IdUser: number,
+        UserName: string,
+        UserCpf: string,
+        UseAdmin: boolean
+    }
+
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    async function fetchUsers() {
+        try {
+            const r = await fetch("http://localhost:3001/api/users");
+
+            if (!r.ok) {
+                throw new Error("Failed to fetch users");
+            }
+
+            const data = await r.json();
+
+            setUsers(data.data);
+            setError(false);
+
+        } catch (error) {
+            console.error(error);
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     useEffect(() => {
-        async function buscaUsuarios(){
-            try{
-
-                const response = await fetch("http://localhost:3001/api/users");
-
-                const data = await response.json();
-
-                setUsers(data);
-
-            }catch(error){
-                console.error("Erro ao buscar usuários:", error);
-            }finally{
-                setLoading(false);
-            }
-        }
-        buscaUsuarios();
+        fetchUsers();
     }, []);
 
-    return(
+    return (
         <div>
-            <div>
-                {users.map((user) => (
-                    <div key={user.UserId}>
-                        <h3>{user.UserName}</h3>
-                        <p>{user.UserCpf}</p>
-                    </div>
-                ))}
-            </div>
+            <h1>Users</h1>
+
+            {loading && <p>Loading...</p>}
+
+            {error && <p>Error fetching users</p>}
+
+            {!loading && !error && (
+                <ul>
+                    {users.map(user => (
+                        <div key={user.IdUser}>
+                            <p>Name: {user.UserName}</p>
+                            <p>CPF: {user.UserCpf}</p>
+                            <p>Admin: {user.UseAdmin == true ? <div className='bg-green-500 text-white w-5 h-5 rounded-2xl'></div> : <div className='bg-red-500 text-white w-5 h-5 rounded-2xl'></div>}</p>
+                        </div>
+                    ))}
+                </ul>
+            )}
         </div>
-    )
+    );
 }
