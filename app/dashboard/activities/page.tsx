aaff0e06728961702849153126763229a9fcc20e
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-06-11 15:29:58 -0300

    feat: estrutra basica da aba atividades

--- app/dashboard/activities/page.tsx
+++ app/dashboard/activities/page.tsx
@@ -1,15 +1,65 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 export default function ActivitiesPage(){
+
+    type Activity = {
+        IdActivities: number,
+        IdUser: number,
+        IdClass: number,
+        ActivitieType: string,
+        ActivitieTitle: string,
+        ActivitieDescription: string
+    }
+
+    const [activities, setActivities] = useState<Activity[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    async function fetchActivities(){
+
+        try{
+
+            const r = await fetch("http://localhost:3001/api/activities");
+
+            if(!r.ok){
+                throw new Error("Failed to fetch activities");
+            }
+
+            const data = await r.json();
+
+            setActivities(data.data);
+
+        }catch(error){
+            console.error(error);
+            setError(true);
+        }finally{
+            setLoading(false);
+        }
+
+    }
+
+    useEffect(() => {
+        fetchActivities();
+    }, []);
+
+
     return(
-        <div>
-            <div>
-                <h1>Atividades</h1>
-                <p>Aqui você pode encontrar todas as atividades entregues e não entregues.</p>
-            </div>
-            <div className="rounded-2xl border border-gray-200 bg-white p-4">
-                <div>
-                    
-                </div>
-            </div>
+        <div> 
+            <h1>Activities</h1>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error fetching activities</p>}
+            {!loading && !error && (
+                <ul>
+                    {activities.map(activity => (
+                        <div key={activity.IdActivities}> 
+                            <h2>{activity.ActivitieTitle}</h2>
+                            <p>{activity.ActivitieDescription}</p>
+                        </div>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
